27d2575a101e254aab8047342d19e92700d75430
visualstudio <visualstudio@122-PC11>
2019-01-25 21:00:47 -0500

    9.00pm

--- C0731847/Program.cs
+++ C0731847/Program.cs
@@ -10,22 +10,39 @@ namespace C0731847
     {
         static void Main(string[] args)
 
-        {}
+        { }
         class Countryside
         {
+            Village Alst;
+            Village Maeland;
+            Village Schenig;
+            public void Initializemap()
+            {
+                 Alst = new Village();
+                Maeland = new Village();
+                Maeland.isAstrildeHere = true;
+                 Schenig = new Village();
 
-        }
+                Alst.NextVillage = Maeland;
+                Maeland.NextVillage = Schenig;
+                Schenig.NextVillage = Alst;
+            }
+            public void WalkAround()
+            {
+                Village initiallVillage = this.Maeland;
+            }
 
-    class Village
-        {
-           Village PreviousVillage;
-           Village NextVillage;
-            String Villagename;
-            bool isAstrildeHere = false;
-            bool isScheningHere = false;
-         
+            class Village
+            {
+                public Village PreviousVillage;
+                public Village NextVillage;
+                public String Villagename;
+                public bool isAstrildeHere = false;
 
-            public  Village() { }
+
+
+                public Village() { }
+            }
         }
     }
 }
